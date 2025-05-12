ad746821ae6cf4992a659e62f0a11c9f4bcf421d
r0nny8000 <r0nny8000@bea2b.de>
2025-05-12 21:30:12 +0200

    Add scripts for Azure Function App installation and deletion

---
+++ run-install-azure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script is used to create an Azure Function App and publish the function to it.
+az login
+az group create --name cb0trg --location germanywestcentral
+az storage account create --name cb0tsto --sku Standard_LRS
+az functionapp create --consumption-plan-location germanywestcentral --runtime python --runtime-version 3.12 --functions-version 4 --name cb0t --os-type linux
+func azure functionapp publish cb0t
