35b5d3a189b5d271bfc673d4e04a1f4675b3c9fd
r0nny8000 <r0nny8000@bea2b.de>
2025-05-19 13:03:04 +0200

    feat: add scripts for creating, deleting, and publishing Azure Function App

--- az-create.sh
+++ az-create.sh
@@ -4,5 +4,4 @@ az login
 az group create --name cb0trg --location germanywestcentral
 az storage account create --name cb0tsto --sku Standard_LRS
 az functionapp create --consumption-plan-location germanywestcentral --runtime python --runtime-version 3.12 --functions-version 4 --name cb0t --os-type linux
-sleep 10
-func azure functionapp publish cb0t
+# func azure functionapp publish cb0t
