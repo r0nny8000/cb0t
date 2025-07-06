7534e2c65f9144492f8d93f477f333b108220718
r0nny8000 <r0nny8000@bea2b.de>
2025-07-06 22:33:28 +0200

    refactor: update variable names for consistency; replace data_daily and data_weekly with df_1d and df_1w in asset class and tests

--- cb0t/asset.py
+++ cb0t/asset.py
@@ -13,16 +13,16 @@ class AssetException(Exception):
 class Asset():
 
     pair = None
-    data_daily = None
-    data_weekly = None
+    df_1d = None
+    df_1w = None
 
     intervals_to_min = {'1m': 1, '5m': 5, '15m': 15, '30m': 30,
                         '1h': 60, '4h': 240, '1d': 1440, '1w': 10080, '2w': 21600}
 
     def __init__(self, pair: str):
         self.pair = pair
-        self.data_daily = self.get_ohlc(pair, '1d', 720)
-        self.data_weekly = self.get_ohlc(pair, '1w', 720)
+        self.df_1d = self.get_ohlc(pair, '1d', 720)
+        self.df_1w = self.get_ohlc(pair, '1w', 720)
 
     def get_ohlc(self, pair: str, interval: str, length: int = 720):
         """Fetches OHLC (Open, High, Low, Close) data for a given currency pair
@@ -62,41 +62,66 @@ class Asset():
 
         return df
 
+    def get_asset_price(self) -> float:
+        """ Fetches the current value for a given currency pair. """
+        try:
+            ticker = Market().get_ticker(self.pair)
+
+            for v in ticker.values():
+                return float(v['c'][0])
+
+        except (KrakenUnknownAssetError, KrakenUnknownAssetPairError) as e:
+            raise AssetException(str(e).replace('\n', ' ')) from e
+
     def calculate_rsi(self, window: int = 14) -> pd.DataFrame:
-        self.data_daily['change'] = self.data_daily['close'].astype(
-            float).diff()
-        self.data_daily['gain'] = self.data_daily.change.mask(
-            self.data_daily.change < 0, 0.0)
-        self.data_daily['loss'] = - \
-            self.data_daily.change.mask(self.data_daily.change > 0, -0.0)
+        self.df_1d['change'] = self.df_1d['close'].astype(float).diff()
+        self.df_1d['gain'] = self.df_1d.change.mask(self.df_1d.change < 0, 0.0)
+        self.df_1d['loss'] = - \
+            self.df_1d.change.mask(self.df_1d.change > 0, -0.0)
 
         # Calculate average gain and loss
-        self.data_daily['avg_gain'] = self.data_daily['gain'].rolling(
+        self.df_1d['avg_gain'] = self.df_1d['gain'].rolling(
             window=window, min_periods=window).mean()
-        self.data_daily['avg_loss'] = self.data_daily['loss'].rolling(
+        self.df_1d['avg_loss'] = self.df_1d['loss'].rolling(
             window=window, min_periods=window).mean()
 
-        self.data_daily['rs'] = self.data_daily.avg_gain / \
-            self.data_daily.avg_loss
-        self.data_daily['rsi'] = 100 - (100 / (1 + self.data_daily.rs))
+        self.df_1d['rs'] = self.df_1d.avg_gain / \
+            self.df_1d.avg_loss
+        self.df_1d['rsi'] = 100 - (100 / (1 + self.df_1d.rs))
 
-        return self.data_daily
+        return self.df_1d
+
+    def calculate_sma(self, window: int = 50) -> pd.DataFrame:
+        """Calculates and returns the Simple Moving Average (SMA) of the data."""
+        column_name = 'sma_' + str(window)
+
+        self.df_1d[column_name] = self.df_1d['close'].astype(float).rolling(
+            window=window, min_periods=window).mean()
+        self.df_1w[column_name] = self.df_1w['close'].astype(float).rolling(
+            window=window, min_periods=window).mean()
+
+        return self.df_1d
 
     def RSI_below(self, threshold: int = 50) -> bool:
         """Returns True if RSI is below the given value."""
 
-        if 'rsi' not in self.data_daily.columns:
+        if 'rsi' not in self.df_1d.columns:
             self.calculate_rsi()
 
-        return self.data_daily['rsi'].iloc[-1] < threshold
+        return self.df_1d['rsi'].iloc[-1] < threshold
 
     def RSI_above(self, threshold: int = 50) -> bool:
         return not self.RSI_below(threshold)
 
-    def Weekly_SMA_below(self, threshold: int = 50) -> bool:
+    def price_below_weekly_SMA(self, window: int) -> bool:
         """Calculates and returns the SMA of the data."""
-        return None
+        column_name = 'sma_' + str(window)
 
-    def Weekly_SMA_above(self, threshold: int = 50) -> bool:
+        if column_name not in self.df_1w.columns:
+            self.calculate_sma(window)
+
+        return self.get_asset_price() < self.df_1w[column_name].iloc[-1]
+
+    def price_above_weekly_SMA(self, window: int) -> bool:
         """Calculates and returns the SMA of the data."""
-        return not self.Weekly_SMA_below(threshold)
+        return not self.price_below_weekly_SMA(window)
--- tests/test_assets.py
+++ tests/test_assets.py
@@ -3,6 +3,15 @@ from cb0t.asset_pairs import *  # pylint: disable=wildcard-import,unused-wildcar
 from cb0t.asset import Asset
 
 
+def print_dataframe(test_name, asset):
+
+    print(f"\n\nTesting {test_name} with pair: {asset.pair}")
+    print("")
+    print(asset.df_1d.tail(4))
+    print("")
+    print(asset.df_1w.tail(4))
+
+
 def test_init():
     """
     Unit tests for the BTCUSD class in the cb0t.btcusd module.
@@ -11,21 +20,70 @@ def test_init():
 
     assert btcusd.pair == "XXBTZUSD"
 
-    assert btcusd.data_daily is not None
-    assert btcusd.data_weekly is not None
-    assert len(btcusd.data_daily) == 720  # defined my value of kraken
-    assert len(btcusd.data_weekly) > 610
+    assert btcusd.df_1d is not None
+    assert btcusd.df_1w is not None
+    assert len(btcusd.df_1d) == 720  # defined my value of kraken
+    assert len(btcusd.df_1w) > 610
+
+    print_dataframe("INIT", btcusd)
 
 
-def test_RSI_above():
+def test_RSI():
     """
-    Test the RSI_above method of the BTCUSD class.
+    Test the RSI method of the BTCUSD class.
     """
     btcusd = BTCUSD()
-    assert 'time' in btcusd.data_daily
-    assert 'close' in btcusd.data_daily
-    assert 'rsi' not in btcusd.data_daily
-    assert btcusd.RSI_below() in [True, False]
-    assert 'rsi' in btcusd.data_daily
-    assert btcusd.data_daily['rsi'].iloc[-1] < 100
-    assert btcusd.data_daily['rsi'].iloc[-1] > 0
+    assert 'time' in btcusd.df_1d
+    assert 'close' in btcusd.df_1d
+    assert 'rsi' not in btcusd.df_1d
+    assert btcusd.calculate_rsi() is not None
+    assert 'rsi' in btcusd.df_1d
+    assert btcusd.df_1d['rsi'].iloc[-1] < 100
+    assert btcusd.df_1d['rsi'].iloc[-1] > 0
+
+    print_dataframe("RSI", btcusd)
+
+    assert btcusd.RSI_below(50) in [True, False]
+    assert btcusd.RSI_above(50) in [True, False]
+
+    rsi = btcusd.df_1d['rsi'].iloc[-1]
+    assert rsi > 0 and rsi < 100
+    assert btcusd.RSI_below(50) == (rsi < 50)
+    assert btcusd.RSI_above(50) == (rsi > 50)
+
+
+def test_SMA():
+    """
+    Test the SMA method of the BTCUSD class.
+    """
+
+    # daily data frame
+    btcusd = BTCUSD()
+    assert 'time' in btcusd.df_1d
+    assert 'close' in btcusd.df_1d
+    assert 'time' in btcusd.df_1w
+    assert 'close' in btcusd.df_1w
+
+    assert 'sma_50' not in btcusd.df_1d
+    assert 'sma_50' not in btcusd.df_1w
+    assert btcusd.calculate_sma(50) is not None
+    assert 'sma_50' in btcusd.df_1d
+    assert 'sma_50' in btcusd.df_1w
+
+    assert 'sma_200' not in btcusd.df_1d
+    assert 'sma_200' not in btcusd.df_1w
+    assert btcusd.calculate_sma(200) is not None
+    assert 'sma_200' in btcusd.df_1d
+    assert 'sma_200' in btcusd.df_1w
+
+    print_dataframe("SMA", btcusd)
+
+    assert btcusd.price_below_weekly_SMA(50) in [True, False]
+    assert btcusd.price_above_weekly_SMA(50) in [True, False]
+
+    price = btcusd.get_asset_price()
+    assert price > 0
+    assert btcusd.price_below_weekly_SMA(50) == (
+        price < btcusd.df_1w['sma_50'].iloc[-1])
+    assert btcusd.price_above_weekly_SMA(50) == (
+        price > btcusd.df_1w['sma_50'].iloc[-1])
