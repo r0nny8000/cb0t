6a96ee63eff2e7e595f5a5daa15ac89868253290
r0nny8000 <r0nny8000@bea2b.de>
2025-08-03 20:12:50 +0200

    style: format comments for consistency in test_function_app.py

--- tests/test_function_app.py
+++ tests/test_function_app.py
@@ -3,12 +3,12 @@ import function_app as app
 from cb0t.asset_pairs import *
 from cb0t.asset import Asset
 
+
 def test_get_balance():
     pass
-    #app.get_balance(req=None)
+    # app.get_balance(req=None)
+
 
 def test_calculate_cost_basis():
     pass
-    #app.calculate_cost_basis(BTCEUR(), 0.00336008)
-
-
+    # app.calculate_cost_basis(BTCEUR(), 0.00336008)
